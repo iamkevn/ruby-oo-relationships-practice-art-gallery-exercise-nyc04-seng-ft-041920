41a054ee4a95fb5e9008cf12d61743a5846d77c5
iamkevn <iamkevnrodriguez@gmail.com>
2020-05-18 11:30:46 -0400

    Mock Code Challenge

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,33 @@ class Artist
 
   attr_reader :name, :years_experience
 
+    @@artists = []
+    @@paintings = []
+    @@galleries = []
+    @@cities = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @artists << self
   end
 
+  def self.artists
+    @artists
+  end
+
+  def self.total_experience
+    @total_experience += self.artists.years_experience  
+  end
+
+  def self.most_prolific
+  end
+
+  def create_painting (title, price, gallery)
+  end
+
+
+
+
 end
+
